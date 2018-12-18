e45126a16c43bb6c3a9e91b0405d22babb767dda
chl9252 <44651541+chl9252@users.noreply.github.com>
2018-12-18 18:55:50 +0500

    comments ready

--- js/check-comment.js
+++ js/check-comment.js
@@ -1,5 +1,54 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');
+	var checkComment = (function(){
+
+		// Переменные модуля
+
+		var _form = $('#comment-add-form'),
+			_commentText = _form.children('.textarea'),
+			_notify = _form.children('.notify');
+
+		// Метод инициализации (запуска) модуля
+
+		var init = function(){
+			_setupListeners();
+
+		};
+
+		// Метод прослушки событий
+
+		var _setupListeners = function(){
+			_form.on('submit', function(event){
+				_formValidate(event);
+
+			});
+		};
+
+		// Приватные методы
+
+		var _formValidate = function(event){
+			event.preventDefault();
+			// proverka
+
+			if(_commentText.val().trim().length == 0) {
+				_notify.fadeIn(700);
+
+			} else {
+				_notify.fadeOut(700);
+				_form.unbind('submit').submit();
+			}
+
+		};
+
+		return {
+			init
+		};
+
+
+	}());
+
+	checkComment.init();
+
+	
 
 });
